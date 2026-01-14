ad127e4f4e4e6e3ee89918ca8fc0e33a7686a358
codebullfrog <66102403+codebullfrog@users.noreply.github.com>
2026-01-14 22:47:20 +1100

    Update __init__.py

--- esphome/components/sonoff_l1/__init__.py
+++ esphome/components/sonoff_l1/__init__.py
@@ -1,10 +1,13 @@
-# esphome/components/sonoff_l1/__init__.py
-
 import esphome.codegen as cg
-from esphome.components import light
+import esphome.config_validation as cv
+from esphome.components import light, uart
+from esphome.const import CONF_ID
 
 sonoff_l1_ns = cg.esphome_ns.namespace("sonoff_l1")
-
-SonoffL1 = sonoff_l1_ns.class_("SonoffL1", cg.Component, light.LightOutput)
+SonoffL1 = sonoff_l1_ns.class_("SonoffL1", cg.Component, light.LightOutput, uart.UARTDevice)
 
 CONF_SONOFF_L1_ID = "sonoff_l1_id"
+
+CONFIG_SCHEMA = light.LIGHT_SCHEMA.extend({
+    cv.GenerateID(CONF_SONOFF_L1_ID): cv.declare_id(SonoffL1),
+}).extend(uart.UART_DEVICE_SCHEMA)
