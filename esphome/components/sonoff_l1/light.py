23e8d568b41d36a9b160bdd96358f1772ef72d6b
codebullfrog <66102403+codebullfrog@users.noreply.github.com>
2026-01-14 21:29:35 +1100

    Update light.py

--- esphome/components/sonoff_l1/light.py
+++ esphome/components/sonoff_l1/light.py
@@ -5,16 +5,18 @@ from esphome.const import CONF_ID
 
 from . import SonoffL1, CONF_SONOFF_L1_ID
 
-CONFIG_SCHEMA = light.LIGHT_SCHEMA.extend({
-    cv.GenerateID(CONF_SONOFF_L1_ID): cv.declare_id(SonoffL1),
-})
+CONFIG_SCHEMA = light.LIGHT_SCHEMA.extend(
+    {
+        cv.GenerateID(CONF_SONOFF_L1_ID): cv.declare_id(SonoffL1),
+    }
+).extend(light.EFFECTS_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_SONOFF_L1_ID])
     await cg.register_component(var, config)
     await light.register_light(var, config)
 
-    # Register effects (Option A)
+    # Register effects
     cg.add(var.add_effect("Colorful Gradient", "gradient"))
     cg.add(var.add_effect("Colorful Breath", "breath"))
     cg.add(var.add_effect("RGB Gradient", "rgb_gradient"))
