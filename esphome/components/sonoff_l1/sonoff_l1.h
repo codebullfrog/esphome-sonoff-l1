d115aafa7a5a95fbf7c71c8c3de0f589c2ca3d94
codebullfrog <66102403+codebullfrog@users.noreply.github.com>
2026-01-14 21:25:36 +1100

    Update sonoff_l1.h

--- esphome/components/sonoff_l1/sonoff_l1.h
+++ esphome/components/sonoff_l1/sonoff_l1.h
@@ -1,22 +1,32 @@
 #pragma once
-#include "esphome.h"
 
+#include "esphome/core/component.h"
+#include "esphome/components/light/light_output.h"
+#include "esphome/components/light/light_effect.h"
+
+namespace esphome {
 namespace sonoff_l1 {
 
-class SonoffL1 : public Component, public LightOutput {
+class SonoffL1 : public Component, public light::LightOutput {
  public:
-  void setup() override;
-  LightTraits get_traits() override;
-  void write_state(LightState *state) override;
+  // Core LightOutput API
+  void write_state(light::LightState *state) override;
 
-  // Effect methods
+  // Effect registration (Option A)
+  void add_effect(const std::string &name, const std::string &id);
+
+  // Serial control helpers (your existing methods)
   void set_mode_gradient();
   void set_mode_breath();
   void set_mode_rgb_gradient();
   void set_mode_rgb_pulse();
   void set_mode_rgb_breath();
   void set_mode_rgb_strobe();
-  void set_mode_sync(int sensitive = 10, int speed = 50);
+  void set_mode_sync();
+
+ protected:
+  std::vector<light::LightEffect *> effects_;
 };
 
 }  // namespace sonoff_l1
+}  // namespace esphome
