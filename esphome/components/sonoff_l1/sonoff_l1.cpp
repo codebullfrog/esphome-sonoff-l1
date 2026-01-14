574b393b5d43641fde1bf25f888185b657488df4
codebullfrog <66102403+codebullfrog@users.noreply.github.com>
2026-01-14 21:58:38 +1100

    Update sonoff_l1.cpp

--- esphome/components/sonoff_l1/sonoff_l1.cpp
+++ esphome/components/sonoff_l1/sonoff_l1.cpp
@@ -1,7 +1,5 @@
 #include "sonoff_l1.h"
 #include "esphome/core/log.h"
-#include "esphome/components/light/effects/lambda.h"
-
 
 namespace esphome {
 namespace sonoff_l1 {
@@ -17,41 +15,19 @@ void SonoffL1::write_state(light::LightState *state) {
   int bi = int(b * 255.0f);
 
   ESP_LOGD(TAG, "Setting RGB: %d %d %d", ri, gi, bi);
+
+  // Example:
   // this->send_at_command("AT+COLOR=%02X%02X%02X", ri, gi, bi);
 }
 
-void SonoffL1::add_effect(const std::string &name, const std::string &id) {
-  auto *effect = new light::LambdaLightEffect(
-      name.c_str(),
-      [this, id](light::LightState *state) {
-        ESP_LOGD(TAG, "Effect triggered: %s", id.c_str());
-
-        if (id == "gradient")
-          this->set_mode_gradient();
-        else if (id == "breath")
-          this->set_mode_breath();
-        else if (id == "rgb_gradient")
-          this->set_mode_rgb_gradient();
-        else if (id == "rgb_pulse")
-          this->set_mode_rgb_pulse();
-        else if (id == "rgb_breath")
-          this->set_mode_rgb_breath();
-        else if (id == "rgb_strobe")
-          this->set_mode_rgb_strobe();
-        else if (id == "sync")
-          this->set_mode_sync();
-      });
-
-  this->effects_.push_back(effect);
-}
-
-void SonoffL1::set_mode_gradient() { ESP_LOGD(TAG, "Setting mode: gradient"); }
-void SonoffL1::set_mode_breath() { ESP_LOGD(TAG, "Setting mode: breath"); }
-void SonoffL1::set_mode_rgb_gradient() { ESP_LOGD(TAG, "Setting mode: rgb_gradient"); }
-void SonoffL1::set_mode_rgb_pulse() { ESP_LOGD(TAG, "Setting mode: rgb_pulse"); }
-void SonoffL1::set_mode_rgb_breath() { ESP_LOGD(TAG, "Setting mode: rgb_breath"); }
-void SonoffL1::set_mode_rgb_strobe() { ESP_LOGD(TAG, "Setting mode: rgb_strobe"); }
-void SonoffL1::set_mode_sync() { ESP_LOGD(TAG, "Setting mode: sync"); }
+// Keep your mode functions if you want them later
+void SonoffL1::set_mode_gradient() { ESP_LOGD(TAG, "gradient"); }
+void SonoffL1::set_mode_breath() { ESP_LOGD(TAG, "breath"); }
+void SonoffL1::set_mode_rgb_gradient() { ESP_LOGD(TAG, "rgb_gradient"); }
+void SonoffL1::set_mode_rgb_pulse() { ESP_LOGD(TAG, "rgb_pulse"); }
+void SonoffL1::set_mode_rgb_breath() { ESP_LOGD(TAG, "rgb_breath"); }
+void SonoffL1::set_mode_rgb_strobe() { ESP_LOGD(TAG, "rgb_strobe"); }
+void SonoffL1::set_mode_sync() { ESP_LOGD(TAG, "sync"); }
 
 }  // namespace sonoff_l1
 }  // namespace esphome
