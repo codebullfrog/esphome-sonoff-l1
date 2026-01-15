2a3f5c812e251c400da8378f087eb7ebf9824bf2
codebullfrog <66102403+codebullfrog@users.noreply.github.com>
2026-01-15 13:17:50 +1100

    Update __init__.py

--- esphome/components/sonoff_l1/__init__.py
+++ esphome/components/sonoff_l1/__init__.py
@@ -6,9 +6,6 @@ from esphome.const import CONF_ID
 sonoff_l1_ns = cg.esphome_ns.namespace("sonoff_l1")
 SonoffL1 = sonoff_l1_ns.class_("SonoffL1", cg.Component, light.LightOutput)
 
-# ⭐ Register the platform name so ESPHome recognizes it
-light.register_light_platform("sonoff_l1", SonoffL1)
-
 CONF_SONOFF_L1_ID = "sonoff_l1_id"
 
 CONFIG_SCHEMA = light.LIGHT_SCHEMA.extend({
