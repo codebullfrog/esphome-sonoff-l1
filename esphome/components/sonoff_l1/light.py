e806b969d2c7312bfa857520d70e862308a7bc84
codebullfrog <66102403+codebullfrog@users.noreply.github.com>
2026-01-15 13:22:35 +1100

    Update light.py

--- esphome/components/sonoff_l1/light.py
+++ esphome/components/sonoff_l1/light.py
@@ -3,10 +3,14 @@ import esphome.config_validation as cv
 from esphome.components import light
 from esphome.const import CONF_ID
 
-from . import SonoffL1, CONF_SONOFF_L1_ID
+from . import SonoffL1
+
+CONF_SONOFF_L1_ID = "sonoff_l1_id"
 
 CONFIG_SCHEMA = light.LIGHT_SCHEMA.extend({
     cv.GenerateID(CONF_SONOFF_L1_ID): cv.declare_id(SonoffL1),
+    # Explicitly allow effects, but don’t enforce a specific schema
+    cv.Optional("effects"): cv.ensure_list(cv.Any()),
 })
 
 async def to_code(config):
