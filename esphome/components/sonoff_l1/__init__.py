2ccbdc2800c7690f46eecc47302aed225150f081
codebullfrog <66102403+codebullfrog@users.noreply.github.com>
2026-01-22 21:45:07 +1100

    Update __init__.py
    using guide

--- esphome/components/sonoff_l1/__init__.py
+++ esphome/components/sonoff_l1/__init__.py
@@ -1,5 +1,23 @@
-import esphome.codegen as cg
-from esphome.components import light
+#pragma
 
-sonoff_l1_ns = cg.esphome_ns.namespace("sonoff_l1")
-SonoffL1 = sonoff_l1_ns.class_("SonoffL1", cg.Component, light.LightOutput)
+#include "esphome/core/component.h"
+
+# Namespace definition
+namespace esphome {
+namespace esphome-sonoff-l1 {
+
+class esphome-sonoff-l1 : public Component {
+
+  public:
+      // Constructor
+      esphome-sonoff-l1();
+
+      void setup() override;
+      void dump_config() override;
+
+  protected:
+    # Internal fields definition
+};
+
+}  //namespace sphome-sonoff-l1
+}  //namespace esphome
