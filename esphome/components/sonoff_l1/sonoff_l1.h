33eb6794c16bc25bd97f4795b6c5feb88b02b48d
codebullfrog <66102403+codebullfrog@users.noreply.github.com>
2026-01-22 22:56:02 +1100

    Update sonoff_l1.h

--- esphome/components/sonoff_l1/sonoff_l1.h
+++ esphome/components/sonoff_l1/sonoff_l1.h
@@ -5,45 +5,48 @@
 #include "esphome/core/log.h"
 #include "Arduino.h"
 
+# Namespace definition
 namespace esphome {
 namespace sonoff_l1 {
 
 class SonoffL1 : public Component, public light::LightOutput {
- public:
-  // Mode constants (same as original)
-  static const int MODE_COLORFUL = 1;
-  static const int MODE_COLORFUL_GRADIENT = 2;
-  static const int MODE_COLORFUL_BREATH = 3;
-  static const int MODE_DIY_GRADIENT = 4;
-  static const int MODE_DIY_PULSE = 5;
-  static const int MODE_DIY_BREATH = 6;
-  static const int MODE_DIY_STROBE = 7;
-  static const int MODE_RGB_GRADIENT = 8;
-  static const int MODE_RGB_PULSE = 9;
-  static const int MODE_RGB_BREATH = 10;
-  static const int MODE_RGB_STROBE = 11;
-  static const int MODE_SYNC_TO_MUSIC = 12;
+//class sonoff_l1 : public Component {
 
-  SonoffL1() = default;
+  public:
 
-  void setup() override;
-  light::LightTraits get_traits() override;
-  void write_state(light::LightState *state) override;
+      // Constructor -- SonoffL1() = default;
+      sonoff_l1();
 
-  // Effect methods (for use from YAML lambdas)
-  void set_mode_gradient();
-  void set_mode_breath();
-  void set_mode_rgb_gradient();
-  void set_mode_rgb_pulse();
-  void set_mode_rgb_breath();
-  void set_mode_rgb_strobe();
-  void set_mode_sync(int sensitive = 10, int speed = 50);
+      void setup() override;
 
- protected:
-  void send_update_(const char *payload);
+      void setup_state(light::LightState *state) override;
+      void write_state(light::LightState *state) override;
+      light::LightTraits get_traits() override;
 
-  bool initialized_{false};
+      void dump_config() override;
+
+      // Effect methods (for use from YAML lambdas)
+      void set_mode_colorful();
+      void set_mode_colorful_gradient();
+      void set_mode_colorful_breath();
+      void set_mode_diy_gradient();
+      void set_mode_diy_pulse();
+      void set_mode_diy_breath();
+      void set_mode_diy_strobe();
+      void set_mode_rgb_gradient();
+      void set_mode_rgb_pulse();
+      void set_mode_rgb_breath();
+      void set_mode_rgb_strobe();
+      void set_mode_sync_to_music(int sensitive = 10, int speed = 50);
+
+  protected:
+    # Internal fields definition
+    void send_update_(const char *payload);
+    bool initialized_{false};
+
+    light::LightState *state_ { nullptr };
+
 };
 
-}  // namespace sonoff_l1
-}  // namespace esphome
+}  //namespace sphome-sonoff-l1
+}  //namespace esphome
