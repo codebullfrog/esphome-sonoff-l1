0fcec55598e18cffa208b0f9b8e8e622da92b1f0
codebullfrog <66102403+codebullfrog@users.noreply.github.com>
2026-01-23 01:04:57 +1100

    Update __init__.py

--- esphome/components/sonoff_l1/light/__init__.py
+++ esphome/components/sonoff_l1/light/__init__.py
@@ -1,6 +1,7 @@
-import esphome.config_validation as cv
 import esphome.codegen as cg
+import esphome.config_validation as cv
 from esphome.components import light, uart
+from esphome.const import CONF_ID
 
 CODEOWNERS = ["@codebullfrog"]
 DEPENDENCIES = ["uart"]
@@ -8,16 +9,14 @@ DEPENDENCIES = ["uart"]
 ns = cg.esphome_ns.namespace("sonoff_l1")
 SonoffL1 = ns.class_("SonoffL1", light.LightOutput, uart.UARTDevice)
 
-CONF_OUTPUT_ID = "output_id"
-
-CONFIG_SCHEMA = light.OUTPUT_SCHEMA.extend({
-    cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(SonoffL1),
+CONFIG_SCHEMA = light.LIGHT_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(SonoffL1),
     cv.Required(uart.CONF_UART_ID): cv.use_id(uart.UARTComponent),
 })
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
-    await cg.register_component(var, config)   # see next note
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
     await light.register_light(var, config)
     uart_component = await cg.get_variable(config[uart.CONF_UART_ID])
     cg.add(var.set_uart_parent(uart_component))
