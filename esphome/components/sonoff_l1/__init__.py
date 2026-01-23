b85dc145b6cf8c9d418078ca604eff37e7326125
codebullfrog <66102403+codebullfrog@users.noreply.github.com>
2026-01-23 12:36:25 +1100

    Update __init__.py with new SonoffL1 component code

--- esphome/components/sonoff_l1/__init__.py
+++ esphome/components/sonoff_l1/__init__.py
@@ -1,1 +1,19 @@
-# Required for ESPHome component discovery
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import uart
+
+CODEOWNERS = ["@codebullfrog"]
+DEPENDENCIES = ["uart"]
+
+sonoff_l1_ns = cg.esphome_ns.namespace("sonoff_l1")
+SonoffL1 = sonoff_l1_ns.class_("SonoffL1", cg.Component, uart.UARTDevice)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(SonoffL1),
+    cv.Required(uart.CONF_UART_ID): cv.use_id(uart.UARTComponent),
+}).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    uart_component = await cg.get_variable(config[uart.CONF_UART_ID])
+    var = cg.new_Pvariable(config[CONF_ID], uart_component)
+    await cg.register_component(var, config)
