252a79b529bea09a205d8b1222310005b54561ab
codebullfrog <66102403+codebullfrog@users.noreply.github.com>
2026-01-23 14:11:49 +1100

    light effects

--- esphome/components/sonoff_l1/light.py
+++ esphome/components/sonoff_l1/light.py
@@ -1,8 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import light, uart
-from esphome.const import CONF_OUTPUT_ID, CONF_ID
-from esphome import automation
+from esphome.const import CONF_OUTPUT_ID, CONF_EFFECTS
 
 CODEOWNERS = ["@codebullfrog"]
 DEPENDENCIES = ["uart"]
@@ -10,19 +9,136 @@ DEPENDENCIES = ["uart"]
 sonoff_l1_ns = cg.esphome_ns.namespace("sonoff_l1")
 SonoffL1 = sonoff_l1_ns.class_("SonoffL1", light.LightOutput, uart.UARTDevice, cg.Component)
 
-# Actions for modes
-SetModeColorfulAction = sonoff_l1_ns.class_("SetModeColorfulAction", automation.Action)
-SetModeColorfulGradientAction = sonoff_l1_ns.class_("SetModeColorfulGradientAction", automation.Action)
-SetModeColorfulBreathAction = sonoff_l1_ns.class_("SetModeColorfulBreathAction", automation.Action)
-SetModeDiyGradientAction = sonoff_l1_ns.class_("SetModeDiyGradientAction", automation.Action)
-SetModeDiyPulseAction = sonoff_l1_ns.class_("SetModeDiyPulseAction", automation.Action)
-SetModeDiyBreathAction = sonoff_l1_ns.class_("SetModeDiyBreathAction", automation.Action)
-SetModeDiyStrobeAction = sonoff_l1_ns.class_("SetModeDiyStrobeAction", automation.Action)
-SetModeRgbGradientAction = sonoff_l1_ns.class_("SetModeRgbGradientAction", automation.Action)
-SetModeRgbPulseAction = sonoff_l1_ns.class_("SetModeRgbPulseAction", automation.Action)
-SetModeRgbBreathAction = sonoff_l1_ns.class_("SetModeRgbBreathAction", automation.Action)
-SetModeRgbStrobeAction = sonoff_l1_ns.class_("SetModeRgbStrobeAction", automation.Action)
-SetModeSyncToMusicAction = sonoff_l1_ns.class_("SetModeSyncToMusicAction", automation.Action)
+# Define effect classes
+ColorfulEffect = sonoff_l1_ns.class_("ColorfulEffect", light.LightEffect)
+ColorfulGradientEffect = sonoff_l1_ns.class_("ColorfulGradientEffect", light.LightEffect)
+ColorfulBreathEffect = sonoff_l1_ns.class_("ColorfulBreathEffect", light.LightEffect)
+DiyGradientEffect = sonoff_l1_ns.class_("DiyGradientEffect", light.LightEffect)
+DiyPulseEffect = sonoff_l1_ns.class_("DiyPulseEffect", light.LightEffect)
+DiyBreathEffect = sonoff_l1_ns.class_("DiyBreathEffect", light.LightEffect)
+DiyStrobeEffect = sonoff_l1_ns.class_("DiyStrobeEffect", light.LightEffect)
+RgbGradientEffect = sonoff_l1_ns.class_("RgbGradientEffect", light.LightEffect)
+RgbPulseEffect = sonoff_l1_ns.class_("RgbPulseEffect", light.LightEffect)
+RgbBreathEffect = sonoff_l1_ns.class_("RgbBreathEffect", light.LightEffect)
+RgbStrobeEffect = sonoff_l1_ns.class_("RgbStrobeEffect", light.LightEffect)
+SyncToMusicEffect = sonoff_l1_ns.class_("SyncToMusicEffect", light.LightEffect)
+
+CONF_SENSITIVE = "sensitive"
+CONF_SPEED = "speed"
+
+@light.register_effect(
+    "sonoff_l1.colorful",
+    ColorfulEffect,
+    "Colorful",
+    {}
+)
+async def colorful_effect_to_code(config, effect_id):
+    return cg.new_Pvariable(effect_id, config["name"])
+
+@light.register_effect(
+    "sonoff_l1.colorful_gradient",
+    ColorfulGradientEffect,
+    "Colorful Gradient",
+    {}
+)
+async def colorful_gradient_effect_to_code(config, effect_id):
+    return cg.new_Pvariable(effect_id, config["name"])
+
+@light.register_effect(
+    "sonoff_l1.colorful_breath",
+    ColorfulBreathEffect,
+    "Colorful Breath",
+    {}
+)
+async def colorful_breath_effect_to_code(config, effect_id):
+    return cg.new_Pvariable(effect_id, config["name"])
+
+@light.register_effect(
+    "sonoff_l1.diy_gradient",
+    DiyGradientEffect,
+    "DIY Gradient",
+    {}
+)
+async def diy_gradient_effect_to_code(config, effect_id):
+    return cg.new_Pvariable(effect_id, config["name"])
+
+@light.register_effect(
+    "sonoff_l1.diy_pulse",
+    DiyPulseEffect,
+    "DIY Pulse",
+    {}
+)
+async def diy_pulse_effect_to_code(config, effect_id):
+    return cg.new_Pvariable(effect_id, config["name"])
+
+@light.register_effect(
+    "sonoff_l1.diy_breath",
+    DiyBreathEffect,
+    "DIY Breath",
+    {}
+)
+async def diy_breath_effect_to_code(config, effect_id):
+    return cg.new_Pvariable(effect_id, config["name"])
+
+@light.register_effect(
+    "sonoff_l1.diy_strobe",
+    DiyStrobeEffect,
+    "DIY Strobe",
+    {}
+)
+async def diy_strobe_effect_to_code(config, effect_id):
+    return cg.new_Pvariable(effect_id, config["name"])
+
+@light.register_effect(
+    "sonoff_l1.rgb_gradient",
+    RgbGradientEffect,
+    "RGB Gradient",
+    {}
+)
+async def rgb_gradient_effect_to_code(config, effect_id):
+    return cg.new_Pvariable(effect_id, config["name"])
+
+@light.register_effect(
+    "sonoff_l1.rgb_pulse",
+    RgbPulseEffect,
+    "RGB Pulse",
+    {}
+)
+async def rgb_pulse_effect_to_code(config, effect_id):
+    return cg.new_Pvariable(effect_id, config["name"])
+
+@light.register_effect(
+    "sonoff_l1.rgb_breath",
+    RgbBreathEffect,
+    "RGB Breath",
+    {}
+)
+async def rgb_breath_effect_to_code(config, effect_id):
+    return cg.new_Pvariable(effect_id, config["name"])
+
+@light.register_effect(
+    "sonoff_l1.rgb_strobe",
+    RgbStrobeEffect,
+    "RGB Strobe",
+    {}
+)
+async def rgb_strobe_effect_to_code(config, effect_id):
+    return cg.new_Pvariable(effect_id, config["name"])
+
+@light.register_effect(
+    "sonoff_l1.sync_to_music",
+    SyncToMusicEffect,
+    "Sync to Music",
+    {
+        cv.Optional(CONF_SENSITIVE, default=10): cv.int_range(min=1, max=100),
+        cv.Optional(CONF_SPEED, default=50): cv.int_range(min=1, max=100),
+    }
+)
+async def sync_to_music_effect_to_code(config, effect_id):
+    effect = cg.new_Pvariable(effect_id, config["name"])
+    cg.add(effect.set_sensitive(config[CONF_SENSITIVE]))
+    cg.add(effect.set_speed(config[CONF_SPEED]))
+    return effect
 
 CONFIG_SCHEMA = light.RGB_LIGHT_SCHEMA.extend(
     {
@@ -35,136 +151,3 @@ async def to_code(config):
     await cg.register_component(var, config)
     await light.register_light(var, config)
     await uart.register_uart_device(var, config)
-
-# Register actions
-SONOFF_L1_ACTION_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.use_id(SonoffL1),
-})
-
-@automation.register_action(
-    "sonoff_l1.set_mode_colorful",
-    SetModeColorfulAction,
-    SONOFF_L1_ACTION_SCHEMA,
-)
-async def set_mode_colorful_to_code(config, action_id, template_arg, args):
-    var = cg.new_Pvariable(action_id, template_arg)
-    await cg.register_parented(var, config[CONF_ID])
-    return var
-
-@automation.register_action(
-    "sonoff_l1.set_mode_colorful_gradient",
-    SetModeColorfulGradientAction,
-    SONOFF_L1_ACTION_SCHEMA,
-)
-async def set_mode_colorful_gradient_to_code(config, action_id, template_arg, args):
-    var = cg.new_Pvariable(action_id, template_arg)
-    await cg.register_parented(var, config[CONF_ID])
-    return var
-
-@automation.register_action(
-    "sonoff_l1.set_mode_colorful_breath",
-    SetModeColorfulBreathAction,
-    SONOFF_L1_ACTION_SCHEMA,
-)
-async def set_mode_colorful_breath_to_code(config, action_id, template_arg, args):
-    var = cg.new_Pvariable(action_id, template_arg)
-    await cg.register_parented(var, config[CONF_ID])
-    return var
-
-@automation.register_action(
-    "sonoff_l1.set_mode_diy_gradient",
-    SetModeDiyGradientAction,
-    SONOFF_L1_ACTION_SCHEMA,
-)
-async def set_mode_diy_gradient_to_code(config, action_id, template_arg, args):
-    var = cg.new_Pvariable(action_id, template_arg)
-    await cg.register_parented(var, config[CONF_ID])
-    return var
-
-@automation.register_action(
-    "sonoff_l1.set_mode_diy_pulse",
-    SetModeDiyPulseAction,
-    SONOFF_L1_ACTION_SCHEMA,
-)
-async def set_mode_diy_pulse_to_code(config, action_id, template_arg, args):
-    var = cg.new_Pvariable(action_id, template_arg)
-    await cg.register_parented(var, config[CONF_ID])
-    return var
-
-@automation.register_action(
-    "sonoff_l1.set_mode_diy_breath",
-    SetModeDiyBreathAction,
-    SONOFF_L1_ACTION_SCHEMA,
-)
-async def set_mode_diy_breath_to_code(config, action_id, template_arg, args):
-    var = cg.new_Pvariable(action_id, template_arg)
-    await cg.register_parented(var, config[CONF_ID])
-    return var
-
-@automation.register_action(
-    "sonoff_l1.set_mode_diy_strobe",
-    SetModeDiyStrobeAction,
-    SONOFF_L1_ACTION_SCHEMA,
-)
-async def set_mode_diy_strobe_to_code(config, action_id, template_arg, args):
-    var = cg.new_Pvariable(action_id, template_arg)
-    await cg.register_parented(var, config[CONF_ID])
-    return var
-
-@automation.register_action(
-    "sonoff_l1.set_mode_rgb_gradient",
-    SetModeRgbGradientAction,
-    SONOFF_L1_ACTION_SCHEMA,
-)
-async def set_mode_rgb_gradient_to_code(config, action_id, template_arg, args):
-    var = cg.new_Pvariable(action_id, template_arg)
-    await cg.register_parented(var, config[CONF_ID])
-    return var
-
-@automation.register_action(
-    "sonoff_l1.set_mode_rgb_pulse",
-    SetModeRgbPulseAction,
-    SONOFF_L1_ACTION_SCHEMA,
-)
-async def set_mode_rgb_pulse_to_code(config, action_id, template_arg, args):
-    var = cg.new_Pvariable(action_id, template_arg)
-    await cg.register_parented(var, config[CONF_ID])
-    return var
-
-@automation.register_action(
-    "sonoff_l1.set_mode_rgb_breath",
-    SetModeRgbBreathAction,
-    SONOFF_L1_ACTION_SCHEMA,
-)
-async def set_mode_rgb_breath_to_code(config, action_id, template_arg, args):
-    var = cg.new_Pvariable(action_id, template_arg)
-    await cg.register_parented(var, config[CONF_ID])
-    return var
-
-@automation.register_action(
-    "sonoff_l1.set_mode_rgb_strobe",
-    SetModeRgbStrobeAction,
-    SONOFF_L1_ACTION_SCHEMA,
-)
-async def set_mode_rgb_strobe_to_code(config, action_id, template_arg, args):
-    var = cg.new_Pvariable(action_id, template_arg)
-    await cg.register_parented(var, config[CONF_ID])
-    return var
-
-CONF_SENSITIVE = "sensitive"
-CONF_SPEED = "speed"
-
-@automation.register_action(
-    "sonoff_l1.set_mode_sync_to_music",
-    SetModeSyncToMusicAction,
-    SONOFF_L1_ACTION_SCHEMA.extend({
-        cv.Optional(CONF_SENSITIVE, default=10): cv.int_range(min=1, max=100),
-        cv.Optional(CONF_SPEED, default=50): cv.int_range(min=1, max=100),
-    }),
-)
-async def set_mode_sync_to_music_to_code(config, action_id, template_arg, args):
-    var = cg.new_Pvariable(action_id, template_arg)
-    await cg.register_parented(var, config[CONF_ID])
-    cg.add(var.set_sensitive(config[CONF_SENSITIVE]))
-    cg.add(var.set_speed(config[CONF_SPEED]))
-    return var
--- esphome/components/sonoff_l1/sonoff_l1.h
+++ esphome/components/sonoff_l1/sonoff_l1.h
@@ -2,9 +2,9 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/light/light_output.h"
+#include "esphome/components/light/light_effect.h"
 #include "esphome/components/uart/uart.h"
 #include "esphome/core/log.h"
-#include "esphome/core/automation.h"
 
 namespace esphome {
 namespace sonoff_l1 {
@@ -44,70 +44,121 @@ class SonoffL1 : public light::LightOutput, public uart::UARTDevice, public Comp
 
 };
 
-// Action classes
-template<typename... Ts> class SetModeColorfulAction : public Action<Ts...>, public Parented<SonoffL1> {
-  public:
-    void play(Ts... x) override { this->parent_->set_mode_colorful(); }
+// Effect classes
+class ColorfulEffect : public light::LightEffect {
+ public:
+  explicit ColorfulEffect(const std::string &name) : LightEffect(name) {}
+  void apply() override {
+    auto *sonoff = static_cast<SonoffL1 *>(this->state_->get_output());
+    sonoff->set_mode_colorful();
+  }
 };
 
-template<typename... Ts> class SetModeColorfulGradientAction : public Action<Ts...>, public Parented<SonoffL1> {
-  public:
-    void play(Ts... x) override { this->parent_->set_mode_colorful_gradient(); }
+class ColorfulGradientEffect : public light::LightEffect {
+ public:
+  explicit ColorfulGradientEffect(const std::string &name) : LightEffect(name) {}
+  void apply() override {
+    auto *sonoff = static_cast<SonoffL1 *>(this->state_->get_output());
+    sonoff->set_mode_colorful_gradient();
+  }
 };
 
-template<typename... Ts> class SetModeColorfulBreathAction : public Action<Ts...>, public Parented<SonoffL1> {
-  public:
-    void play(Ts... x) override { this->parent_->set_mode_colorful_breath(); }
+class ColorfulBreathEffect : public light::LightEffect {
+ public:
+  explicit ColorfulBreathEffect(const std::string &name) : LightEffect(name) {}
+  void apply() override {
+    auto *sonoff = static_cast<SonoffL1 *>(this->state_->get_output());
+    sonoff->set_mode_colorful_breath();
+  }
 };
 
-template<typename... Ts> class SetModeDiyGradientAction : public Action<Ts...>, public Parented<SonoffL1> {
-  public:
-    void play(Ts... x) override { this->parent_->set_mode_diy_gradient(); }
+class DiyGradientEffect : public light::LightEffect {
+ public:
+  explicit DiyGradientEffect(const std::string &name) : LightEffect(name) {}
+  void apply() override {
+    auto *sonoff = static_cast<SonoffL1 *>(this->state_->get_output());
+    sonoff->set_mode_diy_gradient();
+  }
 };
 
-template<typename... Ts> class SetModeDiyPulseAction : public Action<Ts...>, public Parented<SonoffL1> {
-  public:
-    void play(Ts... x) override { this->parent_->set_mode_diy_pulse(); }
+class DiyPulseEffect : public light::LightEffect {
+ public:
+  explicit DiyPulseEffect(const std::string &name) : LightEffect(name) {}
+  void apply() override {
+    auto *sonoff = static_cast<SonoffL1 *>(this->state_->get_output());
+    sonoff->set_mode_diy_pulse();
+  }
 };
 
-template<typename... Ts> class SetModeDiyBreathAction : public Action<Ts...>, public Parented<SonoffL1> {
-  public:
-    void play(Ts... x) override { this->parent_->set_mode_diy_breath(); }
+class DiyBreathEffect : public light::LightEffect {
+ public:
+  explicit DiyBreathEffect(const std::string &name) : LightEffect(name) {}
+  void apply() override {
+    auto *sonoff = static_cast<SonoffL1 *>(this->state_->get_output());
+    sonoff->set_mode_diy_breath();
+  }
 };
 
-template<typename... Ts> class SetModeDiyStrobeAction : public Action<Ts...>, public Parented<SonoffL1> {
-  public:
-    void play(Ts... x) override { this->parent_->set_mode_diy_strobe(); }
+class DiyStrobeEffect : public light::LightEffect {
+ public:
+  explicit DiyStrobeEffect(const std::string &name) : LightEffect(name) {}
+  void apply() override {
+    auto *sonoff = static_cast<SonoffL1 *>(this->state_->get_output());
+    sonoff->set_mode_diy_strobe();
+  }
 };
 
-template<typename... Ts> class SetModeRgbGradientAction : public Action<Ts...>, public Parented<SonoffL1> {
-  public:
-    void play(Ts... x) override { this->parent_->set_mode_rgb_gradient(); }
+class RgbGradientEffect : public light::LightEffect {
+ public:
+  explicit RgbGradientEffect(const std::string &name) : LightEffect(name) {}
+  void apply() override {
+    auto *sonoff = static_cast<SonoffL1 *>(this->state_->get_output());
+    sonoff->set_mode_rgb_gradient();
+  }
 };
 
-template<typename... Ts> class SetModeRgbPulseAction : public Action<Ts...>, public Parented<SonoffL1> {
-  public:
-    void play(Ts... x) override { this->parent_->set_mode_rgb_pulse(); }
+class RgbPulseEffect : public light::LightEffect {
+ public:
+  explicit RgbPulseEffect(const std::string &name) : LightEffect(name) {}
+  void apply() override {
+    auto *sonoff = static_cast<SonoffL1 *>(this->state_->get_output());
+    sonoff->set_mode_rgb_pulse();
+  }
 };
 
-template<typename... Ts> class SetModeRgbBreathAction : public Action<Ts...>, public Parented<SonoffL1> {
-  public:
-    void play(Ts... x) override { this->parent_->set_mode_rgb_breath(); }
+class RgbBreathEffect : public light::LightEffect {
+ public:
+  explicit RgbBreathEffect(const std::string &name) : LightEffect(name) {}
+  void apply() override {
+    auto *sonoff = static_cast<SonoffL1 *>(this->state_->get_output());
+    sonoff->set_mode_rgb_breath();
+  }
 };
 
-template<typename... Ts> class SetModeRgbStrobeAction : public Action<Ts...>, public Parented<SonoffL1> {
-  public:
-    void play(Ts... x) override { this->parent_->set_mode_rgb_strobe(); }
+class RgbStrobeEffect : public light::LightEffect {
+ public:
+  explicit RgbStrobeEffect(const std::string &name) : LightEffect(name) {}
+  void apply() override {
+    auto *sonoff = static_cast<SonoffL1 *>(this->state_->get_output());
+    sonoff->set_mode_rgb_strobe();
+  }
 };
 
-template<typename... Ts> class SetModeSyncToMusicAction : public Action<Ts...>, public Parented<SonoffL1> {
-  public:
-    TEMPLATABLE_VALUE(int, sensitive)
-    TEMPLATABLE_VALUE(int, speed)
-    
-    void play(Ts... x) override { 
-      this->parent_->set_mode_sync_to_music(this->sensitive_.value(x...), this->speed_.value(x...)); 
-    }
+class SyncToMusicEffect : public light::LightEffect {
+ public:
+  explicit SyncToMusicEffect(const std::string &name) : LightEffect(name) {}
+  
+  void set_sensitive(int sensitive) { this->sensitive_ = sensitive; }
+  void set_speed(int speed) { this->speed_ = speed; }
+  
+  void apply() override {
+    auto *sonoff = static_cast<SonoffL1 *>(this->state_->get_output());
+    sonoff->set_mode_sync_to_music(this->sensitive_, this->speed_);
+  }
+  
+ protected:
+  int sensitive_{10};
+  int speed_{50};
 };
 
 }  // namespace sonoff_l1
